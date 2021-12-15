4467b38d0486d72b619e0f8aff62f671cf9982d0
Vincent D <40196601+deakcor@users.noreply.github.com>
2021-12-15 20:48:37 +0100

    fix path with spaces

--- obj-parse/ObjParse.gd
+++ obj-parse/ObjParse.gd
@@ -58,7 +58,7 @@ static func get_mtl_tex_paths(mtl_path:String)->Array:
 		var lines := file.get_as_text().split("\n", false)
 		file.close()
 		for line in lines:
-			var parts = line.split(" ", false)
+			var parts = line.split(" ", false,1)
 			if parts[0] in ["map_Kd","map_Ks","map_Ka"]:
 				if !parts[1] in paths:
 					paths.push_back(parts[1])
